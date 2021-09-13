7b4e4edf8d965e035b9de05b76e0032c9da0cdbf
arturfil <arturfil@hotmail.com>
2021-09-13 15:50:36 -0400

    We left no trace of the morgan package for jeeezus sake

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-// const morgan = require('morgan');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 
@@ -23,7 +22,6 @@ app.use(fileUpload({
   tempFileDir: '/tmp/',
   createParentPath: true
 }));
-// app.use(morgan('dev'));
 
 // routes
 app.use('/api/products', require('./routes/product.js'));
